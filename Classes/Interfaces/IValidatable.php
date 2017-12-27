128624a928a63252743278131f69c93dfe9db46c
Pawe³ Jasiaczyk <pawel.jasiaczyl@przelewy24.pl>
2017-12-27 22:32:14 +0100

    Add comments to IValidate.php

--- Classes/Interfaces/IValidatable.php
+++ Classes/Interfaces/IValidatable.php
@@ -3,9 +3,25 @@ declare(strict_types=1);
 
 namespace PhpMysqlLib;
 
+/**
+ * Interface determines on class ability of validation for potential MySQL error in names etc.
+ */
 interface IValidatable
 {
+    /**
+     * Check if basic data for class is valid.
+     */
     public function isValid() : bool;
+    /**
+     * Check if data in class and all internal objects is valid for all supported MySQL operations.
+     */
+    public function isFullValid() : bool;
+    /**
+     * Validate all fields
+     */
     public function fullValidation() : bool;
+    /**
+     * return report of class validation including reports of internal objects(hierarchicaly).
+     */
     public function validationReport();
 }
